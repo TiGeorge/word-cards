1b1c13f6264a639f43f3638261e3ea37035f5310
TiGeorge <georgetikhomirov@mail.ru>
2020-04-21 14:44:33 +0300

    Initial commit

---
+++ src/main/java/com/example/wordcards/domain/Homograph.java
@@ -0,0 +1,9 @@
+package com.example.wordcards.domain;
+
+import lombok.Data;
+
+@Data
+public class Homograph {
+    Long id;
+    String value;
+}
---
+++ src/main/java/com/example/wordcards/domain/Sense.java
@@ -0,0 +1,9 @@
+package com.example.wordcards.domain;
+
+import lombok.Data;
+
+@Data
+public class Sense {
+    Long id;
+    String value;
+}
